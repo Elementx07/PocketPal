5a6da60a9714d03abf176bc3667b14f45c07a5a5
Jay Shimpi <jayshimpi24@gmail.com>
2025-04-05 17:30:52 +0530

    organize code

--- lib/pages/dash.dart
+++ lib/pages/dash.dart
@@ -1,156 +1,223 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class DashBoardPage extends StatelessWidget {
-    const DashBoardPage({super.key});
+class DashBoardPage extends StatefulWidget {
+  const DashBoardPage({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            backgroundColor: Colors.transparent,
-            extendBodyBehindAppBar: true,
-            extendBody: true,
-            resizeToAvoidBottomInset: false,
-            body: Container(
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                            Color.fromARGB(255, 72, 40, 213),
-                            Color.fromARGB(255, 72, 40, 213),
-                        ],
-                    ),
-                ),
-                child: Column(
-                    children: [
-                    // Top Section
-                    Container(
-                        
-                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-                        decoration: const BoxDecoration(
-                            gradient: LinearGradient(
-                                begin: Alignment.topCenter,
-                                end: Alignment.bottomCenter,
-                                colors: [
-                                    Color.fromARGB(255, 157, 220, 189),
-                                    Color.fromARGB(255, 19, 201, 186),
-                                ],
-                            ),
-                            color: Color.fromARGB(255, 49, 2, 65),
-                            borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(30),
-                                bottomRight: Radius.circular(30),
-                            ),
-                            boxShadow: [
-                                BoxShadow(
-                                    color: Color.fromARGB(255, 8, 60, 58),
-                                    spreadRadius: 0,
-                                    blurRadius: 10,
-                                    offset: Offset(0, 5),
-                                ),
-                            ],
+  @override
+  State<DashBoardPage> createState() => _DashBoardPageState();
+}
 
-                        ),
-                        child: Column(
-                            children: [
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                        
-                                        IconButton(
-                                            icon: const Icon(Icons.notifications, color: Colors.white),
-                                            onPressed: () {},
-                                        ),
-                                    ],
-                                ),
-                                const SizedBox(height: 20),
-                                CircleAvatar(
-                                    radius: 40,
-                                    backgroundImage: AssetImage('assets/user_image.png'), // Replace with your image asset
-                                ),
-                                const SizedBox(height: 10),
-                                const Text(
-                                    "User Name",
-                                    style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-                                ),
-                            ],
-                        ),
-                    ),
-                    const SizedBox(height: 20),                    
-                    const SizedBox(height: 20),
-                    // Bottom Section
-                    SizedBox(
-                      height: 400, // Adjust this value to make it smaller or larger
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Container(
-                            padding: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20),
-                                boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.grey.withOpacity(0.2),
-                                        spreadRadius: 2,
-                                        blurRadius: 5,
-                                        offset: const Offset(0, 3),
-                                    ),
-                                ],
-                            ),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                    const Text(
-                                        "Spending",
-                                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    const Text(
-                                        "₹1436 / ₹3000",
-                                        style: TextStyle(fontSize: 16, color: Colors.blue),
-                                    ),
-                                    const SizedBox(height: 20),
-                                    Expanded(
-                                        child: Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                            children: [
-                                                _buildBar("Mon", 50, Colors.green),
-                                                _buildBar("Tue", 100, Colors.red),
-                                                _buildBar("Wed", 150, Colors.red),
-                                                _buildBar("Thu", 80, Colors.green),
-                                                _buildBar("Fri", 60, Colors.green),
-                                                _buildBar("Sat", 120, Colors.red),
-                                                _buildBar("Sun", 140, Colors.red),
-                                            ],
-                                        ),
-                                    ),
-                                ],
-                            ),
-                        ),
-                    ),
-                  ),
-                ],
-            ),
-        )
-        );
-    }
+class _DashBoardPageState extends State<DashBoardPage> {
+  int _selectedIndex = 0;
 
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Search Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Profile Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Settings Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
 
-    Widget _buildBar(String day, double height, Color color) {
-        return Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-                Container(
-                    width: 10,
-                    height: height,
-                    decoration: BoxDecoration(
-                        color: color,
-                        borderRadius: BorderRadius.circular(5),
-                    ),
-                ),
-                const SizedBox(height: 5),
-                Text(day, style: const TextStyle(fontSize: 12)),
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      extendBodyBehindAppBar: true,
+      extendBody: true,
+      resizeToAvoidBottomInset: false,
+      body: _buildBody(context),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            // Gradient colors for the background
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+                Color.fromARGB(255, 72, 40, 213),
+                Color.fromARGB(255, 72, 40, 213),
             ],
+        ),
+      ),
+      child: ListView(
+        children: [
+          _buildDraggableScrollableSheet(),
+          _buildChart(context),
+          
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDraggableScrollableSheet() {
+        return Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+          child: ListView(
+            children: [
+              const SizedBox(height: 10),
+              Center(
+                child: Container(
+                  width: 50,
+                  height: 5,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(2.5),
+                  ),
+                ),
+              ),
+              _buildProfileSection(),
+            ],
+          ),
         );
-    }
+  }
+
+  Widget _buildProfileSection() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 157, 220, 189),
+            Color.fromARGB(255, 19, 201, 186),
+          ],
+        ),
+        color: Color.fromARGB(255, 49, 2, 65),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(30),
+          bottomRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 8, 60, 58),
+            spreadRadius: 0,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.notifications, color: Colors.white),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          const CircleAvatar(
+            radius: 40,
+            backgroundImage: AssetImage('assets/user_image.png'),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            "User Name",
+            style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildChart(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData(1924, 90, '100%', const Color.fromARGB(255, 219, 130, 41)),
+      ChartData(1925, 50, '100%', const Color.fromARGB(255, 28, 180, 218)),
+      ChartData(1926, 70, '100%', const Color.fromARGB(255, 51, 243, 93)),
+    ];
+
+    return SizedBox(
+      height: 400,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(49, 88, 83, 83),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha(51),
+                spreadRadius: 2,
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(20),
+          child: SfCircularChart(
+            series: <RadialBarSeries<ChartData, int>>[
+              RadialBarSeries<ChartData, int>(
+                useSeriesColor: true,
+                trackOpacity: 0.3,
+                cornerStyle: CornerStyle.bothCurve,
+                dataSource: chartData,
+                pointRadiusMapper: (ChartData data, _) => data.text,
+                pointColorMapper: (ChartData data, _) => data.color,
+                xValueMapper: (ChartData data, _) => data.xValue,
+                yValueMapper: (ChartData data, _) => data.yValue,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.green),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+            backgroundColor: Colors.yellow),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+          backgroundColor: Colors.blue,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings),
+          label: 'Settings',
+          backgroundColor: Colors.red,
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.white,
+      onTap: _onItemTapped,
+    );
+  }
+}
+
+class ChartData {
+  final int xValue;
+  final double yValue;
+  final String text;
+  final Color color;
+
+  ChartData(this.xValue, this.yValue, this.text, this.color);
 }
