e3ad9922641767451e7849747b64a578925cf097
Jay Shimpi <jayshimpi24@gmail.com>
2025-04-07 21:04:30 +0530

    add cosmos background

--- lib/pages/dash.dart
+++ lib/pages/dash.dart
@@ -3,9 +3,10 @@ import 'package:pocket_pal/widgets/bottom_navigation_bar.dart';
 import 'package:pocket_pal/widgets/horizontal_cards.dart';
 import 'package:pocket_pal/widgets/profile_section.dart';
 import 'package:pocket_pal/widgets/radial_chart.dart';
+import 'package:rive/rive.dart';
 
 class DashBoardPage extends StatefulWidget {
-  const DashBoardPage({super.key});
+  const DashBoardPage({Key? key}) : super(key: key);
 
   @override
   State<DashBoardPage> createState() => _DashBoardPageState();
@@ -35,7 +36,17 @@ class _DashBoardPageState extends State<DashBoardPage> {
       extendBodyBehindAppBar: true,
       extendBody: true,
       resizeToAvoidBottomInset: false,
-      body: _buildBody(context),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: RiveAnimation.asset(
+              'assets/animations/cosmos.riv',
+              fit: BoxFit.cover,
+            ),
+          ),
+          _buildBody(context),
+        ],
+      ),
       bottomNavigationBar: PocketPalBottomNavigationBar(
         selectedIndex: _selectedIndex,
         onItemTapped: _onItemTapped,
@@ -46,60 +57,18 @@ class _DashBoardPageState extends State<DashBoardPage> {
   Widget _buildBody(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        color: Color.fromARGB(255, 78, 6, 123),
+        color: Colors.transparent,
       ),
       child: Column(
         children: [
-          GestureDetector(
-            onVerticalDragUpdate: (details) {
-              if (details.delta.dy > 5 && !_isProfileVisible) {
-                // Swipe down
-                setState(() {
-                  _isProfileVisible = true;
-                });
-              } else if (details.delta.dy < -5 && _isProfileVisible) {
-                // Swipe up
-                setState(() {
-                  _isProfileVisible = false;
-                });
-              }
+          ProfileSection(
+            isProfileVisible: _isProfileVisible,
+            onVisibilityChanged: (visible) {
+              setState(() {
+                _isProfileVisible = visible;
+              });
             },
-            child: ProfileSection(
-              isProfileVisible: _isProfileVisible,
-              onVisibilityChanged: (visible) {
-                setState(() {
-                  _isProfileVisible = visible;
-                });
-              },
-            ),
           ),
-          if (!_isProfileVisible)
-            GestureDetector(
-              onVerticalDragUpdate: (details) {
-                if (details.delta.dy > 5) {
-                  setState(() {
-                    _isProfileVisible = true;
-                  });
-                }
-              },
-              child: Container(
-                height: 100,
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 49, 2, 65),
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withAlpha(51),
-                      spreadRadius: 2,
-                      blurRadius: 10,
-                      offset: const Offset(0, 5),
-                    ),
-                  ],
-                ),
-                alignment: Alignment.center,
-                child: const Icon(Icons.arrow_drop_down, color: Colors.white),
-              ),
-            ),
           Expanded(
             child: LayoutBuilder(
               builder: (BuildContext context, BoxConstraints viewportConstraints) {
