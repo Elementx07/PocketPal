19077e3e651e0c1938fbf7c577f4def8f2ff401c
Jay Shimpi <98270472+Elementx07@users.noreply.github.com>
2025-10-06 19:33:15 +0530

    Hackathon (#10)
    * Updated main.dart to simplify content by removing provider functionality and expense/subscription providers.
    
    * Remove provider dependencies and replace with static sample data for hackathon participants.
    
    * Remove ExpenseProvider dependency and replace with static sample data for hackathon participants

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 import 'package:pocket_pal/pages/splash_screen.dart';
-import 'package:pocket_pal/util/streak_provider.dart';
-import 'package:pocket_pal/providers/expense_provider.dart';
-import 'package:pocket_pal/providers/subscription_provider.dart'; // Import SubscriptionProvider
-
 
 void main() {
   // Ensure bindings are initialized before using plugins or SystemChrome
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => StreakCoinProvider()
-          ..loadData()
-          ..updateStreakWithDate()),
-        ChangeNotifierProvider(create: (_) => ExpenseProvider()),
-        ChangeNotifierProvider(create: (_) => SubscriptionProvider()), // Add this provider
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
